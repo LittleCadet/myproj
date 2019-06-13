79056d1cb0be3701d2424ba3f912dc24f9ef1464
shenxie <sx255879@163.com>
2019-06-13 19:02:30 +0800

    schedule的对比

--- src/main/java/com/myproj/app/MyprojApplication.java
+++ src/main/java/com/myproj/app/MyprojApplication.java
@@ -2,7 +2,11 @@ package com.myproj.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Date;
+
+@EnableScheduling
 @SpringBootApplication
 public class MyprojApplication {
 

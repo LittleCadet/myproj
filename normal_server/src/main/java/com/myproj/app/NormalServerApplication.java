319113e2a6d7e18add663ad196d519b8f045301b
LittleCadet <sx255879@163.com>
2019-07-10 22:56:37 +0800

    '让springBoot启动就开始运行定时任务'

--- normal_server/src/main/java/com/myproj/app/NormalServerApplication.java
+++ normal_server/src/main/java/com/myproj/app/NormalServerApplication.java
@@ -2,8 +2,10 @@ package com.myproj.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NormalServerApplication {
 
 	public static void main(String[] args) {

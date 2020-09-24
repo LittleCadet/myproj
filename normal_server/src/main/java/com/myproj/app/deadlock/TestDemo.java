40c7601440517e042a5a944e8d539ba43a5e81fb
shenxie <shenxie@msitong.com>
2020-09-24 21:26:33 +0800

    InitializingBean : 是在bean加载的时候，执行

---
+++ normal_server/src/main/java/com/myproj/app/deadlock/TestDemo.java
@@ -0,0 +1,39 @@
+package com.myproj.app.deadlock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * InitializingBean : 是在bean加载的时候，执行
+ *
+ * @author shenxie
+ * @date 2020/9/24
+ */
+@Service
+public class TestDemo implements InitializingBean{
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("======InitializingBean开始执行死锁=======");
+        DeadThread dt1 = new DeadThread();
+        dt1.setFlag("a");
+        Thread t1 = new Thread(dt1);
+        t1.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        dt1.setFlag("b");
+        Thread t2 = new Thread(dt1);
+
+        t2.start();
+    }
+}

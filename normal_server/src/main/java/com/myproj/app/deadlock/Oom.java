8c067d2f893250c02f0c71376bbf4164ecdc200f
shenxie <shenxie@msitong.com>
2020-09-30 15:56:32 +0800

    添加oom代码

---
+++ normal_server/src/main/java/com/myproj/app/deadlock/Oom.java
@@ -0,0 +1,34 @@
+package com.myproj.app.deadlock;
+
+import com.google.common.collect.Maps;
+import com.myproj.app.copy.Copy;
+import org.checkerframework.checker.units.qual.K;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shenxie
+ * @date 2020/9/25
+ */
+@Service
+public class Oom implements InitializingBean {
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        int i = 0;
+
+        Map<Integer, Integer> map = new HashMap(1 , 0.0000000001f);
+
+
+
+
+        while(true){
+            map.put((int)(Math.random()*Integer.MAX_VALUE) , (int)(Math.random()*Integer.MAX_VALUE) );
+            System.out.println(String.format("第 【%s】 次防止" , i++));
+        }
+    }
+}

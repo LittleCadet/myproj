fb629aebaaa0de8bacd6cbe3ec2a3d4a719d660e
shenxie <shenxie@msitong.com>
2021-06-28 06:10:33 +0800

    零宽断言

--- normal_server/src/main/java/com/myproj/app/string/Matcher.java
+++ normal_server/src/main/java/com/myproj/app/string/Matcher.java
@@ -48,22 +48,17 @@ public class Matcher {
     }
 
     private static void showInfo() {
-        String str ="# <font color=#0099ff size=2>【liveservice】指定接口响应时间过长！</font>\n" +
+        String str ="# <font color=#0099ff size=2>【ksearch-api】指定接口响应时间过长！<>\n" +
                 "----\n" +
-                "![avatar](https://kkl-static.oss-cn-hangzhou.aliyuncs.com/alert/slow3.jpg)\n" +
+                "![avatar](slow3.jpg)\n" +
                 "\n" +
-                "应用liveservice最近2分钟内接口响应时间196次超过阈值1ms(1次)!\n" +
+                "应用ksearch-api最近2分钟内接口响应时间104次超过阈值500ms(10次)!\n" +
                 "\n" +
-                "2021-05-17 16:43:00到16:45:00如下接口响应时间超过阈值1次：\n" +
-                ">LiveShowPrivateFacade:selectLiveShowSimpleById -- 58次 平均1.0ms\n" +
-                "LiveClassManagerFacade:getClassStudentInfo -- 48次 平均15.0ms\n" +
-                "LiveCourseQueryFacade:getOnlineNumber -- 38次 平均1.4ms\n" +
-                "LiveRankingFacade:getAllClassAttendanceRank -- 24次 平均50.0ms\n" +
-                "JobInvoker:executeAll -- 9次 平均1.0ms\n" +
-                "等等\n";
+                "2021-05-22 00到00如下接口响应时间超过阈值10次：\n" +
+                ">/app/logs/upload/v2 -- 103次 平均3506.4ms";
         // 正则的零宽断言
         String regex = "(?<=响应时间).*?(?=次)";
-        String regex3 = "([a-z0-9]*\\.){2}([a-z0-9]*)|([a-z0-9]*\\:{1}([a-z0-9]*))|(?<=-- ).*?(?=次)|(?<=平均).*?(?=ms)";
+        String regex3 = "([a-z0-9]*\\.){2}([a-z0-9]*)|([a-z0-9]*\\:{1}([a-z0-9]*))|(?<=-- ).*?(?=次)|(?<=平均).*?(?=ms)|(\\/([a-z0-9]*))*";
 //        String regex3 = "([a-z0-9]*\\:{1}([a-z0-9]*))";
 //        String regex4 = "^(?!.*helloworld).*$";
         Pattern p = compile(regex);
@@ -73,8 +68,9 @@ public class Matcher {
             System.out.println("应用超时总次数：" + m.group());
         }
         String s1 = str.replaceAll("([0-9]{2})\\:", "");
-        System.out.println("result:" + s1);
-        String s = s1.replaceAll("https://kkl-static.oss-cn-hangzhou.aliyuncs.com", "");
+        String s = s1.replaceAll("(https://kkl-static.oss-cn-hangzhou.aliyuncs.com|严重:|/font|/alert/|https://user-images.githubusercontent.com/9434884/43697219-3cb4ef3a-9975-11e8-9a9c-73f4f537442d.png)", "");
+
+        System.out.println("result:" + s);
         Pattern compile1 = compile(regex3,  CASE_INSENSITIVE);
         java.util.regex.Matcher m3 = compile1.matcher(s);
         while(m3.find()){

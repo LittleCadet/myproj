d6f3140281369f8b4395cf54f82df86b5e845953
LittleCadet <sx255879@163.com>
2024-09-19 23:14:05 +0800

    斐的代码

--- excel_server/src/main/java/com/excel/server/MFei.java
+++ excel_server/src/main/java/com/excel/server/MFei.java
@@ -4,24 +4,24 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
 import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.compress.utils.Sets;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class MFei {
     public static void main(String[] args) {
-//        String filePath = "C:\\Users\\sx255\\Desktop\\样表20240914 - 副本.xlsx";
-//        String outputPath = "C:\\Users\\sx255\\Desktop\\样表20240914 - 输出.xlsx";
-        if(args.length != 2) {
-            System.out.println("参数不是2个, 请检查参数:" + Arrays.toString(args));
-            return;
-        }
-        String filePath = args[0];
-        String outputPath = args[1];
+        String filePath = "C:\\Users\\sx255\\Desktop\\样表20240914 - 副本.xlsx";
+        String outputPath = "C:\\Users\\sx255\\Desktop\\样表20240914 - 输出.xlsx";
+//        if(args.length != 2) {
+//            System.out.println("参数不是2个, 请检查参数:" + Arrays.toString(args));
+//            return;
+//        }
+//        String filePath = args[0];
+//        String outputPath = args[1];
         // sheet页 => sheet页中的所有行记录
         List<List<LinkedHashMap<Integer, String>>> totalDatas = Lists.newArrayList();
+        // 所有的sheet页中的项目名
+        Set<String> totalProjects = Sets.newHashSet();
         List<LinkedHashMap<Integer, String>> finalDatas = new LinkedList<>();
         long start = System.currentTimeMillis();
         try {
@@ -44,6 +44,7 @@ public class MFei {
                                         rowData.remove(columns--);
                                     }
                                     datas.add(rowData);
+                                    totalProjects.add(rowData.get(1));
                                 }
                             }
 
@@ -68,41 +69,21 @@ public class MFei {
             // 将第一个sheet页中的数据按照项目名 填充。
             final int[] count = {0};
             if (totalDatas.size() > 1) {
-                totalDatas.get(0).forEach(sheet -> {
-                    sheet.forEach((k, v) -> {
-                        // 根据项目名匹配
-                        if (k == 1) {
-                            for (int i = 0; i < totalDatas.size(); i++) {
-                                int finalI = i;
-                                totalDatas.get(finalI).parallelStream().filter(data -> data.containsValue(v)).forEach(data -> {
-//                                        if (finalDatas.stream().anyMatch(da -> da.containsValue(v))) {
-//                                            finalDatas.stream().filter(finalData -> finalData.containsValue(v)).forEach(finalData -> {
-//                                                // 将非第一页的序号 + 项目名： 放入finalData中。
-//                                                for (int j = 2; j < data.size(); j++) {
-//                                                    finalData.put(finalData.size(), data.get(j));
-//                                                }
-//                                            });
-//                                        } else {
-//                                            finalDatas.add(new LinkedHashMap<>(data));
-//                                        }
+                totalProjects.forEach(project -> {
+                    for (int i = 0; i < totalDatas.size(); i++) {
+                        // 该项目在该年份中是否存在
+                        final boolean[] isExistInYear = {false};
+                        // 该项目在该年份中存在，则用该年份中的项目数据来填充finalDatas.
+                        existInYear(isExistInYear, project, totalDatas, finalDatas, i);
 
-                                    final boolean[] flag = {false};
-                                    finalDatas.parallelStream().filter(finalData -> finalData.containsValue(v)).forEach(finalData -> {
-                                        // 将非第一页的序号 + 项目名： 放入finalData中。
-                                        for (int j = 2; j < data.size(); j++) {
-                                            finalData.put(finalData.size(), data.get(j));
-                                        }
-                                        flag[0] = true;
-                                    });
-                                    if (!flag[0]) {
-                                        finalDatas.add(new LinkedHashMap<>(data));
-                                    }
-                                });
-                            }
-                            System.out.println(">>>>>>数据处理：" + ++count[0] + "完成");
-                        }
-                    });
+                        // 该项目在该年份中不存在， 则用null来填充finalDatas.
+                        nonExistInYear(isExistInYear, project, finalDatas);
+
+
+                    }
+                    System.out.println(">>>>>>数据处理[项目]：" + ++count[0] + "完成");
                 });
+
             } else {
                 System.out.println("只包含一个sheet页的数据， 不做处理");
             }
@@ -123,5 +104,48 @@ public class MFei {
             return false;
         }
         return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private static void existInYear(boolean[] isExistInYear, String project, List<List<LinkedHashMap<Integer, String>>> totalDatas, List<LinkedHashMap<Integer, String>> finalDatas, int index ){
+        totalDatas.get(index).parallelStream().filter(data -> data.containsValue(project)).forEach(data -> {
+            // 该项目在finalDatas中是否存在
+            final boolean[] flag = {false};
+            isExistInYear[0] = true;
+            finalDatas.parallelStream().filter(finalData -> finalData.containsValue(project)).forEach(finalData -> {
+                // 将非第一页的序号 + 项目名： 放入finalData中。
+                for (int j = 2; j < data.size(); j++) {
+                    finalData.put(finalData.size(), data.get(j));
+                }
+                flag[0] = true;
+            });
+            if (!flag[0]) {
+                finalDatas.add(new LinkedHashMap<>(data));
+            }
+        });
+    }
+
+    private static void nonExistInYear(boolean[] isExistInYear, String project, List<LinkedHashMap<Integer, String>> finalDatas){
+        if( ! isExistInYear[0]) {
+            final boolean[] flag = {false};
+            finalDatas.parallelStream().filter(finalData -> finalData.containsValue(project)).forEach(finalData -> {
+                flag[0] = true;
+                // 将非第一页的序号 + 项目名： 放入finalData中。
+                for (int j = 2; j < 9; j++) {
+                    finalData.put(finalData.size(), null);
+                }
+            });
+            if(! flag[0]){
+                LinkedHashMap<Integer, String> nullData = new LinkedHashMap<>();
+                for (int j = 0; j < 9; j++) {
+                    if(j == 1) {
+                        // 保存项目名
+                        nullData.put(j, project);
+                    }else{
+                        nullData.put(j, null);
+                    }
+                }
+                finalDatas.add(nullData);
+            }
+        }
     }
 }
